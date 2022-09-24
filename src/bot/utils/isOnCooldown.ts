89e24d8cc4042167678efee6d3de931585497001
Matthew Smilansky <msmilansky@gmail.com>
2022-09-24 00:52:48 -0700

    Added return statement to a conditional within the isOnCooldown fn.

--- src/bot/utils/isOnCooldown.ts
+++ src/bot/utils/isOnCooldown.ts
@@ -5,7 +5,7 @@ const { GLOBAL_COOLDOWN_TIME_IN_SECONDS } = require('./../../constants');
 
 exports.isOnCooldown = async (username: string, command: string) => {
   console.log(username);
-  // Try to find a cooldown with the provided username and command name in the database. If such a cooldown isn't found, create a new cooldown with the provided username and command name and set the startTime field to the current time in milliseconds.
+  // Try to find a cooldown with the provided username and command name in the database. If such a cooldown isn't found, create a new cooldown with the provided username and command name and set the startTime field to the current time in milliseconds, then return false, indicating that there was not a cooldown at the time the command was issued.
   try {
     let cooldown = await Cooldown.findOne({
       username: username.toLowerCase(),
@@ -18,14 +18,10 @@ exports.isOnCooldown = async (username: string, command: string) => {
         command,
         startTime: new Date(Date.now()).getTime()
       });
+      return false;
     }
-    //
-    console.log(Math.floor(cooldown.startTime / 1000));
-    // Store the current time in milliseconds in a variable.
-    const now = new Date(Date.now()).getTime();
 
-    console.log(Math.floor(new Date(Date.now()).getTime() / 1000));
-    // if the current time is not equal to the cooldown's startTime, and if 30 seconds hasn't passed since the cooldown's startTime, return true, indicated that there is an active cooldown. Otherwise, delete the current cooldown, as it has expired, and return false, indicating that there is no active cooldown.
+    // if the current time is not equal to the cooldown's startTime, and if 30 seconds hasn't passed since the cooldown's startTime, return true, indicating that there is an active cooldown. Otherwise, delete the current cooldown, as it has expired, and return false, indicating that there is no active cooldown.
     if (
       convertMsToSec(getCurrentTimeInMs()) !==
         convertMsToSec(cooldown.startTime) &&
@@ -35,7 +31,7 @@ exports.isOnCooldown = async (username: string, command: string) => {
     ) {
       return true;
     } else {
-      await Cooldown.deleteOne({ username: username, command: command });
+      await Cooldown.deleteOne({ username, command });
       cooldown = await Cooldown.create({
         username,
         command,
