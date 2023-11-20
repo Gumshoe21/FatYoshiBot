eb61a94304698bbc91bb5ee4a211fd099f805bb1
Matthew Smilansky <msmilansky@gmail.com>
2023-11-20 10:24:23 -0800

    fix: remove TAU

--- src/bot/handlers.ts
+++ src/bot/handlers.ts
@@ -1,78 +1,99 @@
-import tmiClient from './tmiClient.js'
-import { commands } from './commands.js'
-import { rewards } from './rewards.js'
-import isOnCooldown from './utils/isOnCooldown.js'
-import { isCommand } from './../helpers/isCommand.js'
-import { setAsyncInterval } from './../helpers/setAsyncInterval.js'
-import { FAT_YOSHI_TIMER_INTERVAL } from './../constants.js'
-import { Client, ChatUserstate } from 'tmi.js'
-import commandRegexp from './../helpers/commandRegexp.js'
-import dotenv from 'dotenv'
-import fetch, { Headers } from 'node-fetch'
-import { getUserId, timeoutUser } from './../utils/twitchApi.js'
-import { ApiClient } from '@twurple/api'
+import tmiClient from "./tmiClient.js";
+import { commands } from "./commands.js";
+import { rewards } from "./rewards.js";
+import isOnCooldown from "./utils/isOnCooldown.js";
+import { isCommand } from "./../helpers/isCommand.js";
+import { setAsyncInterval } from "./../helpers/setAsyncInterval.js";
+import { FAT_YOSHI_TIMER_INTERVAL } from "./../constants.js";
+import { Client, ChatUserstate } from "tmi.js";
+import commandRegexp from "./../helpers/commandRegexp.js";
+import dotenv from "dotenv";
+import fetch, { Headers } from "node-fetch";
+import { getUserId, timeoutUser } from "./../utils/twitchApi.js";
 
-import { UserIdResolvable } from '@twurple/api'
-
-dotenv.config()
-export async function commandHandler(this: Client, channel, context, message, self) {
+dotenv.config();
+export async function commandHandler(
+  this: Client,
+  channel,
+  context,
+  message,
+  self,
+) {
   try {
-    if (!isCommand(message)) return
+    if (!isCommand(message)) return;
 
     // If the user is the bot itself, exit fn.
-    if (self) return
+    if (self) return;
 
     // Separate the raw message, the command itself, and any args into variables.
-    const [raw, command, argument] = message.match(commandRegexp)
+    const [raw, command, argument] = message.match(commandRegexp);
 
     // Obtain the onCommand fn from the command to which it belongs.
-    const { onCommand } = commands[command] || {}
+    const { onCommand } = commands[command] || {};
 
-    if (!commands[command]) return
+    if (!commands[command]) return;
 
-    let response = await onCommand(channel, context, message, self)
+    let response = await onCommand(channel, context, message, self);
 
     // Execute the isOnCooldown fn and await its response, which will be either true if there is an active cooldown or false if there isn't one.
-    const cooldownIsActive = await isOnCooldown(context.username, command)
+    const cooldownIsActive = await isOnCooldown(context.username, command);
 
-    if (typeof response !== 'string') {
-      if (response['timeout'] === true) {
-        await this.say(channel, response!['say'])
+    if (typeof response !== "string") {
+      if (response["timeout"] === true) {
+        await this.say(channel, response!["say"]);
 
-        const { userId: botUserId } = await getUserId(this.getUsername())
+        const { userId: botUserId } = await getUserId(this.getUsername());
 
-        const channelId = context!['room-id']
-        const moderatorId = context!['room-id']
-        const userId = context!['user-id']
-        const { duration, reason } = response
+        const channelId = context!["room-id"];
+        const moderatorId = context!["room-id"];
+        const userId = context!["user-id"];
+        const { duration, reason } = response;
 
-        const { data } = await timeoutUser(channelId, botUserId, userId, duration, reason)
+        const { data } = await timeoutUser(
+          channelId,
+          botUserId,
+          userId,
+          duration,
+          reason,
+        );
 
-        console.log(data)
+        console.log(data);
       }
     }
 
-    if (typeof response === 'string') {
+    if (typeof response === "string") {
       if (!cooldownIsActive) {
-        this.say(channel, response)
+        this.say(channel, response);
       }
     }
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
-export async function rewardHandler(channel: string, username: string, type: string, tags: ChatUserstate): Promise<void> {
-  if (!rewards[type]) return
+export async function rewardHandler(
+  channel: string,
+  username: string,
+  type: string,
+  tags: ChatUserstate,
+): Promise<void> {
+  if (!rewards[type]) return;
   // Obtain the onReward fn from the reward to which it belongs.
-  const { onReward } = rewards[type] || {}
+  const { onReward } = rewards[type] || {};
 
   // Execute the onReward fn, await its response, and store it in a var.
-  let response = await onReward({ channel, username, type, tags })
+  let response = await onReward({ channel, username, type, tags });
 
-  tmiClient.say(channel, response)
+  tmiClient.say(channel, response);
 }
 
-export const raidHandler = async (raidedChannel, raiderUsername, raidViewerCount) => {
-  tmiClient.say(raidedChannel, `${raiderUsername} is raiding your channel with ${raidViewerCount} delicious viewers! Dinner is served!`)
-}
+export const raidHandler = async (
+  raidedChannel,
+  raiderUsername,
+  raidViewerCount,
+) => {
+  tmiClient.say(
+    raidedChannel,
+    `${raiderUsername} is raiding your channel with ${raidViewerCount} delicious viewers! Dinner is served!`,
+  );
+};
--- src/server.ts
+++ src/server.ts
@@ -1,54 +1,40 @@
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import tmiClient from './bot/tmiClient.js'
-import { commandHandler, rewardHandler, raidHandler } from './bot/handlers.js'
-import app from './app.js'
-import WebSocket from 'ws'
-dotenv.config()
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import tmiClient from "./bot/tmiClient.js";
+import { commandHandler, rewardHandler, raidHandler } from "./bot/handlers.js";
+import app from "./app.js";
+import WebSocket from "ws";
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI || '').then((_con) => console.log('Database connection successful.'))
+mongoose
+  .connect(process.env.MONGO_URI || "")
+  .then((_con) => console.log("Database connection successful."));
 
-const tauSocketUrl = `ws://localhost:${process.env.TAU_PORT}/ws/twitch-events/`
-console.log('TAU Socket URL:', tauSocketUrl)
+await tmiClient.connect();
 
-const ws = new WebSocket(tauSocketUrl)
-
-ws.on('open', () => {
-  console.log('Socket Open:', tauSocketUrl)
-
-  ws.send(JSON.stringify({ token: process.env.TAU_AUTH_TOKEN }))
-  console.log("TAU connected and listening for events...")
-})
-
-ws.on('message',(data) => {
-  const eventData = JSON.parse(data);
-  console.log('--- message', eventData)
-})
-await tmiClient.connect()
-
-tmiClient.on('message', commandHandler)
+tmiClient.on("message", commandHandler);
 // tmiClient.on('redeem', rewardHandler)
-tmiClient.on('raided', raidHandler)
-tmiClient.on('timeout', async (channel, username, duration, reason) => {
-  console.log(channel, username, duration, reason)
-})
+tmiClient.on("raided", raidHandler);
+tmiClient.on("timeout", async (channel, username, duration, reason) => {
+  console.log(channel, username, duration, reason);
+});
 
-process.on('uncaughtException', (err) => {
-  console.log(err)
-  process.exit(1) // code 0 = success; code 1 = uncaught exception
-})
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit(1); // code 0 = success; code 1 = uncaught exception
+});
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => {})
+const server = app.listen(port, () => {});
 
-process.on('unhandledRejection', (err) => {
-  console.log(err)
+process.on("unhandledRejection", (err) => {
+  console.log(err);
   server.close(() => {
-    process.exit(1)
-  })
-})
+    process.exit(1);
+  });
+});
 
-process.on('SIGTERM', () => {
-  server.close(() => {})
-})
+process.on("SIGTERM", () => {
+  server.close(() => {});
+});
